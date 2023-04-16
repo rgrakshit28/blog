1597670644d9cbc539919228923632317c6da77c
rgrakshit28 <rgrakshit28.professional@gmail.com>
2023-04-16 09:03:22 +0530

    UI Register and Login Page Design

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Single from './pages/Single';
 import Write from './pages/Write';
+import './style.scss';
 
 const Layout = () => {
   return (
@@ -49,8 +50,9 @@ function App() {
 
   return (
     <div className="app">
-      <div className="container"></div>
-      <RouterProvider router={router} />
+      <div className="container">
+        <RouterProvider router={router} />
+      </div>
     </div>
   );
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="auth">
+      <h1>Login</h1>
+      <form>
+        <input type="text" placeholder="username" required />
+        <input type="password" placeholder="password" required />
+        <button>Login</button>
+        <p>This is an error!</p>
+        <span>
+          Dont you have an account? <Link to="/register">Register</Link>
+        </span>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
